30ca6024a3b04231d36a461ddfb055a638920413
Pedrog93 <pedro.gc93@gmail.com>
2018-11-14 23:52:17 -0600

    Done

--- plantilla-floodit.py
+++ plantilla-floodit.py
@@ -12,12 +12,29 @@ if len(sys.argv)==1 :
 	print("Favor de proporcionar el txt con codigo de colores")
 
 else:
-	print (file.read())
+	#print (file.read())
 	texto=file.read()
 	colores={"a":"#ffff00","c":"#00ffff","d":"#ffc90e","m":"#800080","r":"#ff0000","v":"#00bb00"}
+	dic='acdmrv'
+	x0=0
+	x1=20
+	y0=0
+	y1=20
+	for i in texto:
+		if i in colores.keys():
+			#print(colores[i])
+			c.create_rectangle(x0,y0,x1,y1,fill=colores[i],outline="")
+			x0+=20
+			x1+=20
+			#print(x0,x1)
+		else:
+		 	if "\n" in texto:
+		 		x0=0
+		 		x1=20
+		 		y0+=20
+		 		y1+=20
 	#Codigo ejemplo para crear un rectangulo
-	c.create_rectangle(0,0,20,20,fill="#ff0000",outline="")
-
-	c.pack()
-	c.mainloop()
+	
+c.pack(side= 'left')
+c.mainloop()
 	
